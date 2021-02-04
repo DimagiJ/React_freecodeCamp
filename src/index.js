a569c81a55099351f57c665c24ccf53de801b94b
Chathuranga Costa <it17088138@my.sliit.lk>
2021-02-04 17:58:49 +0530

    Made 2 Book components as a constant

--- src/index.js
+++ src/index.js
@@ -1,17 +1,57 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDom from 'react-dom'
+import './index.css'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const firstBook = {
+    img: 'https://www.india.com/wp-content/uploads/2016/12/One-Indian-Girl.jpg',
+    title: "One Indian Girl",
+    author : "Chetan Bhagat"
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+}
+
+const secondBook = {
+    img: 'https://images-na.ssl-images-amazon.com/images/I/41bChkOa4zL._AC_UL200_SR200,200_.jpg',
+    title: "Think Again: The Power of Knowing",
+    author : "Adam Grant"
+
+}
+
+
+
+
+const BookList = () => {
+    return (
+        <section className='booklist'>
+            <Book img={firstBook.img}
+                  title={firstBook.title}
+                  author={firstBook.author} />
+
+                  <Book img={secondBook.img}
+                        title={secondBook.title}
+                        author={secondBook.author} ><p>Think Again is a must-read for anyone
+                                                        who wants to create a culture of learning
+                                                        and exploration, whether at home, at work,
+                                                        or at school... In an increasingly divided
+                                                        world, the lessons in this book are more important than ever.</p></Book>
+        </section>
+    )
+}
+
+//BOOK Object
+const Book = (props) => {
+    const {img, title, author, children} = props
+    return( <article className='book'>
+            <img className='img' src={img} alt='' />
+            <h1>{title}</h1>
+            {children}
+            <h4 style={{color:'#617d98', fontSize:'0.75rem'
+                , margin:'0.25rem'}}>{author}</h4>
+
+
+
+        </article>
+        )
+};
+
+
+ReactDom.render(<BookList />, document.getElementById('root'));
